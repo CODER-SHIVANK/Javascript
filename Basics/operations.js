f31d5ba6588d091775b11548f50dcf1103b6e284
CODER-SHIVANK <HASHTAGSHIVANKCODER@GMAIL.COM>
2024-06-13 02:31:09 +0000

    discussion on basic operations

---
+++ Basics/operations.js
@@ -0,0 +1,53 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " shivank silot"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 2);
+
+console.log((3+4) * 3 % 4);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 =2 + 2
+
+// Postfix Operations
+let a = 100
+let b = a++
+console.log(a);
+console.log(b);
+
+let x = 100
+let y = x--
+console.log(x);
+console.log(y);
+
+// Prefix Operations 
+let c = 100
+let d = ++c
+console.log(c);
+console.log(d);
+
+let p = 100 
+let q = --p
+console.log(p);
+console.log(q);
