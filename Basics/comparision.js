843ab5272737fb8f0845384894bad13a3ced6d9b
CODER-SHIVANK <HASHTAGSHIVANKCODER@GMAIL.COM>
2024-06-13 02:55:15 +0000

    discussion on comparision of datatypes

---
+++ Basics/comparision.js
@@ -0,0 +1,22 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("2a" < "2b");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// Double equal operator is used to compare the value 
+// whereas triple equal operator is used to compare the value as well its datatypes.
+
+console.log("2" === 2);
